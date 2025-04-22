bb076cfafb489fc7ab061e405d42984272c29e37
ge48hid <ge48hid@gitlab.lrz.de>
2025-04-22 10:35:25 +0200

    nextPermutation

--- LeetCode31.java
+++ LeetCode31.java
@@ -50,7 +50,8 @@ public class LeetCode31 {
 //  Hence first we have to find the right most number that is smaller than the next one, which is gonna be our pivot.
 //  This is gonna be swaped with the smallest number on the right that is still larger than the pivot.
 //  How can we be sure that the the right most number is the smallest one that is still larger than the pivot?
-//  We can figure from the last step of the algorithm. The array starting from the pivot + 1 is already sorted.
+//  We can figure from the last step of the algorithm. The array starting from the pivot + 1 is gonna be descending, since the pivot is the last number that is
+//  still smaller that its successor.
 //  That means the right most number is the smallest one that is still larger than the pivot.
 //  Then we reverse the whole array from the pivot + 1 to the end.
 
