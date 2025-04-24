b08150f9eaceebe4c81ad765b686694ffffe156f
ge48hid <ge48hid@gitlab.lrz.de>
2025-04-25 00:19:04 +0200

    longestValidParentheses

--- LeetCode32.java
+++ LeetCode32.java
@@ -8,15 +8,15 @@ public class LeetCode32 {
         System.out.println(leetcode.longestValidParentheses("()(()"));
     }
 
-    // TODO: increment counter if see ( and decrement if see ). Ignore ) when counter is negative. Consider appending
-    //  counter into a test string and perhaps use a separator to know differentiate between 1, 0 and 10.
+    // TODO: use a stack to keep track of the index of the opening bracket. Every time we see a closing bracket, we pop the index of the opening bracket.
+    //  To ensure we also include the opening bracket when the stack is empty, we have to push the buffer index onto the stack!
     public int longestValidParentheses(String s) {
         Stack<Integer> stack = new Stack<>();
         int maxLength = 0;
         for (int i = 0; i < s.length(); i++) {
-            // The closing brackets will consume the index of the opening bracket. '
-            // So to include the may be consumed opening bracket when the stack is empty,
-            // we have to push the buffer index onto the stack!
+            // TODO: The closing brackets will consume the index of the opening bracket.
+            //  So to include the may be consumed opening bracket when the stack is empty,
+            //  we have to push the buffer index onto the stack!
             if (stack.isEmpty() && s.charAt(i) == '(') {
                 stack.push(i - 1);
                 stack.push(i);
@@ -34,3 +34,38 @@ public class LeetCode32 {
         return maxLength;
     }
 }
+
+// TODO: Another approach - Two Traversals: The idea is to solve the problem using two traversals of the string, one from left to right and one from right to left,
+//  while keeping track of the number of open and close parentheses using two counters: open and close. Why there is a Two Traversals?
+//  Left to right traversal ensures that every valid substring that ends at the rightmost closing parenthesis is counted.
+//  Right to left traversal ensures that every valid substring that starts from the leftmost opening parenthesis is counted.
+//        int maxLen = 0;
+//        // Left to Right Traversal
+//        int open = 0, close = 0;
+//        for (char ch : s.toCharArray()) {
+//            if (ch == '(') {
+//                open++;
+//            } else if (ch == ')') {
+//                close++;
+//            }
+//            if (open == close) {
+//                maxLen = Math.max(maxLen, 2 * close);
+//            } else if (close > open) {
+//                open = close = 0;
+//            }
+//        }
+//        // Right to Left Traversal
+//        open = close = 0;
+//        for (int i = s.length() - 1; i >= 0; i--) {
+//            if (s.charAt(i) == '(') {
+//                open++;
+//            } else if (s.charAt(i) == ')') {
+//                close++;
+//            }
+//            if (open == close) {
+//                maxLen = Math.max(maxLen, 2 * open);
+//            } else if (open > close) {
+//                open = close = 0;
+//            }
+//        }
+//        return maxLen;
