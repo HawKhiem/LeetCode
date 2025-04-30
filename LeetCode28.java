dd55dfa2ae09f220b59a6b2f8978c82725576fa3
ge48hid <ge48hid@gitlab.lrz.de>
2025-04-30 23:55:11 +0200

    day 4 leetcode

--- LeetCode28.java
+++
@@ -1,6 +0,0 @@
-public class LeetCode28 {
-    public int strStr(String haystack, String needle) {
-        if (haystack.contains(needle)) return haystack.indexOf(needle);
-        return -1;
-    }
-}
