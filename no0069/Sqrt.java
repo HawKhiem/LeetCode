022899d11f50016e4967f48afc21286bc8ab1141
Khiem Nguyen <ge48hid@tum.de>
2025-05-06 21:35:22 +0200

    square root using binary search

--- no0069/Sqrt.java
+++ no0069/Sqrt.java
@@ -9,11 +9,24 @@ public class Sqrt {
     }
 
     public int mySqrt(int x) {
-        int exponent = 0;
-        while (x > 0) {
-            x /= 2;
-            exponent++;
+        int left = 0, right = x;
+        int result = 0;
+        while (left <= right) {
+            /// This avoids overflow
+            int mid = left + (right - left) / 2;
+            /// Too big
+            if (Math.pow(mid, 2) > x) {
+                right = mid - 1;
+                result = mid - 1;
+            }
+            /// Too small
+            else if (Math.pow(mid, 2) < x) {
+                left = mid + 1;
+                result = mid;
+            } else {
+                return mid;
+            }
         }
-        return exponent;
+        return result;
     }
 }
