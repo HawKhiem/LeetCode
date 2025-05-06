495bda35320c88e39c98f150a948bf0bbd429abc
Khiem Nguyen <ge48hid@tum.de>
2025-05-07 00:33:40 +0200

    set zero

--- no0073/SetMatrixZeroes.java
+++ no0073/SetMatrixZeroes.java
@@ -1,4 +1,24 @@
 package no0073;
 
 public class SetMatrixZeroes {
+    public void setZeroes(int[][] matrix) {
+        /// use two arrays to track down which row and which column should be set to 0
+        int[] row = new int[matrix.length];
+        int[] col = new int[matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
 }
