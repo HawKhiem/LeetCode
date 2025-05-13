780974d482722a2cf86ea9173190046d3865b64d
Khiem Nguyen <ge48hid@tum.de>
2025-05-13 17:16:27 +0200

    binary tree level order with BFS

--- BinarySearchTree/no102/BinaryTreeLevelOrderTraversal.java
+++ BinarySearchTree/no102/BinaryTreeLevelOrderTraversal.java
@@ -1,4 +1,51 @@
 package BinarySearchTree.no102;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class BinaryTreeLevelOrderTraversal {
+    // TODO - Breadth First Search
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int qLength = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < qLength; i++) {
+                TreeNode node = queue.poll();
+                if (node != null) {
+                    level.add(node.val);
+                    queue.add(node.left);
+                    queue.add(node.right);
+                }
+            }
+            if (!level.isEmpty()) {
+                result.add(level);
+            }
+        }
+        return result;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
