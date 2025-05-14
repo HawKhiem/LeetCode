2664cfb9cf6ff1011cde8f4b4a202077ade2987a
Khiem Nguyen <ge48hid@tum.de>
2025-05-14 20:57:44 +0200

    change structure

---
+++ BFS/no0107/BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,54 @@
+package BFS.no0107;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeLevelOrderTraversalII {
+    // TODO - use BFS - FIFO (First in first out)
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int queueLength = queue.size();
+            ///  deal with one level
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < queueLength; i++) {
+                TreeNode node = queue.poll();
+                if (node != null) {
+                    queue.add(node.left);
+                    queue.add(node.right);
+                    level.add(node.val);
+                }
+            }
+            if (!level.isEmpty()) {
+                result.add(0, level);
+            }
+        }
+        return result;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
