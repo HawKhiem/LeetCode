9b505c403de1b0002c9cbf0f9b04f6890bc995f2
Khiem Nguyen <ge48hid@tum.de>
2025-05-16 00:38:20 +0200

    best time to buy and sell stock 2

--- NOTYETCATEGORIZED/no0122/BestTimeToBuyAndSellStockII.java
+++ NOTYETCATEGORIZED/no0122/BestTimeToBuyAndSellStockII.java
@@ -1,4 +1,16 @@
 package NOTYETCATEGORIZED.no0122;
 
 public class BestTimeToBuyAndSellStockII {
+
+    // TODO: always buy low sell high (which means buying on the rising trend)
+    //  Intuition: https://www.youtube.com/watch?v=3SJ3pUkPQMc
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += (prices[i] - prices[i - 1]);
+            }
+        }
+        return profit;
+    }
 }
